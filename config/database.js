166ac3a9e26775e12c9ed7438582e4c0fb51d8a7
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2025-08-05 12:55:12 +0700

    Update for vercel

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('db_homeline', 'root', '', {
-    host: "localhost",
-    dialect: "mysql"
-})
-
-// const db = new Sequelize('db_homeline', 'lukman', 'LukmanGaming', {
-//     host: "202.10.36.184",
+// const db = new Sequelize('db_homeline', 'root', '', {
+//     host: "localhost",
 //     dialect: "mysql"
 // })
+
+const db = new Sequelize('db_homeline', 'lukman', 'LukmanGaming', {
+    host: "202.10.36.184",
+    dialect: "mysql"
+})
 
 
 export default db;
