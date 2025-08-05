84ec2421a0fa0b4c89f053aaca3fdd50d113b209
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2025-08-06 02:46:04 +0700

    Update prepare up to vercel

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.use(
     })
 );
 
+app.options('*', cors());
+
 // (async() => {
 //     await db.sync();
 // })();
@@ -68,6 +70,9 @@ app.use(
 );
 
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
 app.use(fileUpload({
     createParentPath: true,
     limits: {
@@ -76,8 +81,6 @@ app.use(fileUpload({
     abortOnLimit: true,
     responseOnLimit: "File size limit has been reached",
 }));
-
-app.use(express.json());
 
 app.use((req, res, next) => {
     console.log(`[${req.method}] ${req.originalUrl}`);
@@ -90,13 +93,6 @@ app.use("/api/designer", DesignerRoute);
 app.use("/api/admin/level-users", LevelUserRoute);
 app.use("/api/auth", AuthRoute);
 app.use("/api/user", UserRoute);
-
-app.use((err, req, res, next) => {
-    if (err.status === 413) {
-        return res.status(413).json({ message: "Ukuran file terlalu besar" });
-    }
-    next(err);
-});
 
 // store.sync();
 
