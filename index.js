eca313f4a180bfc5cfbadbc8719bf59bbe55771a
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2025-08-06 12:16:13 +0700

    Update bug 3d

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ app.use(
     })
 );
 
-app.use(express.json());
 app.use(fileUpload({
     createParentPath: true,
     limits: {
@@ -76,6 +75,7 @@ app.use(fileUpload({
     abortOnLimit: true,
     responseOnLimit: "File size limit has been reached",
 }));
+app.use(express.json());
 
 
 app.use((req, res, next) => {
