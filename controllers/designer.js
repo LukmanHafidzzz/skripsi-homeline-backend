6fb50d3fdafa3f3c1d8331ecab4f8f1bad811afa
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2025-08-06 14:08:09 +0700

    Update prepare up to vercel

--- controllers/designer.js
+++ controllers/designer.js
@@ -331,6 +331,10 @@ export const getPresignedUrlForDesign = async (req, res) => {
 
 export const saveDesignFileInfo = async (req, res) => {
     try {
+        console.log("SaveDesignFileInfo dipanggil");
+        console.log("Data body:", req.body);
+        console.log("Session userId:", req.session.userId);
+
         const { house_id, fileUrl, fileName } = req.body;
 
         if (!house_id || !fileUrl || !fileName) {
@@ -355,11 +359,13 @@ export const saveDesignFileInfo = async (req, res) => {
             });
         }
 
-        await HouseDesigns.create({
+        const newDesign = await HouseDesigns.create({
             house_id: house_id,
             user_id: user.id,
             design_file: fileUrl
         });
+
+        console.log("Berhasil menyimpan HouseDesigns:", newDesign);
 
         await HouseProcesses.update(
             {
