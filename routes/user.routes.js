b0ea350f14563b403d08decdf8b4f9d5c3330362
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2026-02-26 20:29:34 +0700

    Fixing vercel bug: max size upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,7 +12,6 @@ import {
     deleteHouseByUser,
     getHouseAdvertisementDetail,
     getHouseModelAdvertisement,
-    generatePresignedUrls,
     addHouse,
     getSearchHouse,
     getSearchHouseDetail,
