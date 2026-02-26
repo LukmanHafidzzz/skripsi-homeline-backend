3e0c406691aece854aa5b298733106ca88e59998
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2026-02-26 20:49:01 +0700

    Fixing vercel bug: max size upload

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ Handle preflight OPTIONS secara eksplisit
-app.options('*', cors(corsOptions));
+app.options('/(.*)', cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
