0dd2eb3beb5811a1ab19d21b0d2f9e5e0e5db8d1
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2026-02-26 20:26:43 +0700

    Fixing vercel bug: max size upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -44,7 +44,6 @@ router.delete('/advertisement/delete/:id', deleteHouseByUser)
 router.get('/advertisement/detail/:id', getHouseAdvertisementDetail);
 router.get('/advertisement/detail/model/:id', getHouseModelAdvertisement);
 
-router.post('/advertisement/presigned-url', generatePresignedUrls);
 router.post('/advertisement/add', addHouse);
 
 router.get('/provinces', getProvinces);
