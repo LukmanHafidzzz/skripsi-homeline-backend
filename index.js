4f1a524261e86392961e129b673e75b2e85a4323
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2026-02-28 16:02:53 +0700

    Removing test api

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ import LevelUserRoute from "./routes/level.user.routes.js";
 import SurveyorRoute from './routes/surveyor.routes.js';
 import DesignerRoute from './routes/designer.routes.js';
 import AuthRoute from "./routes/auth.routes.js";
-import TestRoute from "./routes/test.routes.js";
 import fileUpload from "express-fileupload";
 
 dotenv.config();
@@ -92,7 +91,6 @@ app.use("/api/designer", DesignerRoute);
 app.use("/api/admin/level-users", LevelUserRoute);
 app.use("/api/auth", AuthRoute);
 app.use("/api/user", UserRoute);
-app.use("/api/tests", TestRoute);
 // store.sync();
 
 const PORT = process.env.APP_PORT
